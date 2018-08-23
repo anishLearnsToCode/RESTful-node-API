5de5206baff303ad27218db97b5cb81f7cea010c
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-08-23 15:23:44 +0530

    initial commit in the master

---
+++ index.js
@@ -0,0 +1,32 @@
+// Primary File for the API
+
+
+// Importing Modules & declaring constants
+const http = require('http');
+const url = require('url');
+const portAddress = 3000;
+
+// The server should respond to all the requests that are sent to it with a string
+const server = http.createServer( (request, response) => {
+
+    //Create a URL and parse it
+    let parsedURL = url.parse(request.url, true);
+    console.log(parsedURL);
+
+    //Get the path from the URL
+    let URLPath = parsedURL.pathname;
+    console.log(URLPath);
+    let trimmedPath = URLPath.trim();
+
+    //Send the response to the conjugate request
+
+
+    //Log the response
+
+    response.end('hello world \n' + request.url);
+});
+
+// The server should listen on port 3000
+server.listen(portAddress, () => {
+    console.log('the server is listening on port ' + portAddress);
+});
