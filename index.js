077b5a6335903595bc4e5fa10697f7d7649a3a4b
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-08-25 23:09:25 +0530

    loggig multile characteristics of the response including the headers and the method of the request

--- index.js
+++ index.js
@@ -11,21 +11,31 @@ const server = http.createServer( (request, response) => {
 
     //Create a URL and parse it
     let parsedURL = url.parse(request.url, true);
-    console.log(parsedURL);
+
 
     //Get the path from the URL
     let URLPath = parsedURL.pathname.replace(/^\/+|\/+$/g, '');
-    console.log(URLPath);
+
 
     //Send the response to the conjugate request
     response.end('hello world');
 
-    //Log the response
-    console.log('The URL path requested is : ' + URLPath);
+
+    //Logging the response
+    //Logging the response URL Path
+    console.log('The URL path requested is : ' + URLPath );
+
+    // Logging the request headers
+    console.log('The request headers sent to the server is ');
+    console.log(request.headers);
+
+    // Logging teh request method
+    console.log(request.method);
+    console.log('--------------------------');
 
 });
 
 // The server should listen on port 3000
 server.listen(portAddress, () => {
-    console.log('the server is listening on port ' + portAddress);
+    console.log('the server is listening on port ' + portAddress + '\n');
 });
